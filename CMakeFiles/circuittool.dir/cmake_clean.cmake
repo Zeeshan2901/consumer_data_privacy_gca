2082dbc3e6dd58f5a7ab711b81174d5a6ce0b8c4
zeeshan2901 <syedzeeshanahmed@live.in>
2019-08-26 02:31:12 -0300

    init

---
+++ CMakeFiles/circuittool.dir/cmake_clean.cmake
@@ -0,0 +1,10 @@
+file(REMOVE_RECURSE
+  "CMakeFiles/circuittool.dir/test/circuittool.cpp.o"
+  "bin/circuittool.pdb"
+  "bin/circuittool"
+)
+
+# Per-language clean rules from dependency scanning.
+foreach(lang CXX)
+  include(CMakeFiles/circuittool.dir/cmake_clean_${lang}.cmake OPTIONAL)
+endforeach()
